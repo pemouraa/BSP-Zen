97d6a821e03611b453aa8e09bbaa43143f6d5889
Pedro Moura <pedro030707@gmail.com>
2025-05-21 22:09:32 -0300

    Adicionando forma de pagamento

--- Plano/plano.js
+++ Plano/plano.js
@@ -1,7 +1,30 @@
 function selecionarPlano(plano) {
-      if (plano === 'Gratuito') {
+    if (plano === 'Gratuito') {
         window.location.href = '../Gratuito/gratuito.html';
-      } else if (plano === 'Premium') {
-        window.location.href = '../Premium/premium.html'
-      }
     }
+}
+
+function mostrarModalPagamento() {
+    const modal = document.getElementById('modalPagamento');
+    modal.style.display = 'flex';
+}
+
+function selecionarPagamento(metodo) {
+    console.log(`Pagamento selecionado: ${metodo}`);
+    window.location.href = '../Premium/premium.html';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('modalPagamento');
+    const fechar = document.querySelector('.fechar');
+
+    fechar.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
